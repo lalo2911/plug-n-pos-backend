a97b3d8b8d81244b8a4b0fdb384a781ed3232b2e
lalo2911 <luiseduardotorres-29@hotmail.com>
2025-03-19 17:42:14 -0600

    end basic crud of category

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,8 +4,8 @@ import { ProductController } from '../controllers/productController.js';
 const router = Router();
 const productController = new ProductController();
 
+router.post('/', productController.createProduct);
 router.get('/', productController.getProducts);
 router.get('/:id', productController.getProduct);
-router.post('/', productController.createProduct);
 
 export default router;
