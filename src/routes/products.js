1a8e93da319ba331f0a96b382c2d3d12da3ce629
lalo2911 <luiseduardotorres-29@hotmail.com>
2025-03-19 21:37:58 -0600

    end basic crud of products

--- src/routes/products.js
+++ src/routes/products.js
@@ -7,5 +7,7 @@ const productController = new ProductController();
 router.post('/', productController.createProduct);
 router.get('/', productController.getProducts);
 router.get('/:id', productController.getProduct);
+router.put('/:id', productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
 
 export default router;
