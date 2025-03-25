13e55e73357238bac1af6f7d16a30ede18ebcf8b
lalo2911 <luiseduardotorres-29@hotmail.com>
2025-03-25 17:12:09 -0600

    updt order model

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -1,24 +1,25 @@
 import mongoose from 'mongoose';
 
 const orderSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please add a name'],
-        trim: true
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please add a user']
     },
-    price: {
+    total: {
         type: mongoose.Types.Decimal128,
         required: [true, 'Please add a price'],
         min: 0
     },
-    image_url: {
-        type: String,
-        default: null
+    payment: {
+        type: mongoose.Types.Decimal128,
+        required: [true, 'Please add a payment'],
+        min: 0
     },
-    category_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category',
-        required: [true, 'Please add a category']
+    change: {
+        type: mongoose.Types.Decimal128,
+        required: [true, 'Please add a change'],
+        min: 0
     }
 }, {
     timestamps: true
