552c158d828c85bfdacfd3ac5ce8ad4122f57c71
lalo2911 <luiseduardotorres-29@hotmail.com>
2025-06-23 12:12:07 -0600

    minor changes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -295,10 +295,10 @@ export class AuthController {
     setRefreshTokenCookie(res, refreshToken) {
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'lax',
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 días
-            path: '/'
+            secure: true,
+            sameSite: 'none',
+            path: '/',
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 días
         });
     }
 }
